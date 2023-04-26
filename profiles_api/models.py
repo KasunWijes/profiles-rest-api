84b1992ce5b2f74ca44ed9476e25debb3c5c4d7a
Kasun <kasunpamodya@gmail.com>
2023-04-26 12:59:10 +0200

    Added profiles viewset

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -24,8 +24,8 @@ class UserProfileManager(BaseUserManager):
         """Create and save a new superuser with given details"""
         user = self.create_user(email, name, password)
 
-        user.is_supperuser =True
-        user.is_staff=True
+        user.is_superuser = True
+        user.is_staff = True
         user.save(using=self._db)
 
         return user
