1b864e5fa3931e10db4bf19822bbf1827967a93c
Kasun <kasunpamodya@gmail.com>
2023-05-20 23:37:33 +0200

    added user profile feed API

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -3,4 +3,5 @@ from django.contrib import admin
 from profiles_api import models
 
 admin.site.register(models.UserProfile)
+admin.site.register(models.ProfileFeedItem)
 # Register your models here.
